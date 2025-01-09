65d57dff1a811bf2ab22248286b19664d36f86c0
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-01-09 07:41:29 +0700

    Lab 1 Cau 2

---
+++ src/Java1/Lab1/Cau2.java
@@ -0,0 +1,21 @@
+package Java1.Lab1;
+
+import java.util.Scanner;
+
+/* Viết chương trình nhập từ bàn phím 2 cạnh của hình chữ nhật. Tính và xuất chu vi,
+diện tích và cạnh nhỏ của hình chữ nhật. */
+
+public class Cau2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap chieu dai cua hinh chu nhat:");
+        Double chieudai = scanner.nextDouble();
+        System.out.println("Nhap chieu rong cua hinh chu nhat:");
+        Double chieurong = scanner.nextDouble();
+        Double chuvi = ( chieudai + chieurong ) * 2;
+        Double dientich = chieudai * chieurong;
+        System.out.printf("Chu vi HCN:\n" + chuvi);
+        System.out.printf("\nDien tich HCN:\n" + dientich);
+        scanner.close();
+    }
+}
