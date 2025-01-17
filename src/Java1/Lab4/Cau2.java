3b24ec3444ad4f694e951f2474de8bdd758e7ef0
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-01-17 23:29:02 +0700

    Cau 2 Lab 4

---
+++ src/Java1/Lab4/Cau2.java
@@ -0,0 +1,52 @@
+package Java1.Lab4;
+
+import java.util.Scanner;
+
+/* Viết chương trình tạo ra 2 sản phẩm thông tin được nhập từ bàn phím sau đó gọi
+phương thức xuất để xuất thông tin 2 đối tượng sản phẩm đã tạo
+HƯỚNG DẪN:
+MOB1014 – LẬP TRÌNH JAVA 1
+TRANG 1
+LAB4: LỚP VÀ ĐỐI TƯỢNG
+✓ Tạo lớp chứa phương thức main()
+✓ Trong phương thức main() tạo 2 đối tượng sp1 và sp2 từ lớp sản phẩm
+✓ Gọi phương thức nhap() của 2 đối tượng sp1 và sp2 để nhập dữ liệu từ bàn
+phím
+✓ Gọi phương thức xuat() của 2 đối tượng sp1 và sp2 để xuất thông tin của
+mỗi đối tượng ra màn hình
+Name : Chau Nhat Duy
+MSSV: PS44284
+Date: 17/1/2025
+ */
+public class Cau2 {
+    public static class Product {
+        public String nameProduct;
+        public double price, giamGia, thue;
+
+        public class Product(String nameProduct, double price, giamGia, thue) {
+            this.nameProduct = nameProduct;
+
+        }
+
+        public void inputProduct(){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Nhap thong tin san pham:");
+            nameProduct = sc.nextLine();
+            System.out.print("Nhap gia cua san pham:");
+            price = sc.nextDouble();
+            System.out.print("Giam gia?:");
+            giamGia = sc.nextDouble();
+            System.out.print("Nhap thue san pham ( % ):");
+            thue = sc.nextDouble();
+        }
+
+        public double tinhTienThue(double price, double thue) {
+            thue = price * 0.1;
+            return thue;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product product = new Product;
+    }
+}
